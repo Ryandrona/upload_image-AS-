bff35f044699ba73dd8d6f5006408622c6e6f9c7
TruongPhuDong <onlyonetime279@gmail.com>
2022-10-30 20:45:50 +0700

    upload

--- app/src/main/java/com/example/manager_user/HomeActivity.java
+++ app/src/main/java/com/example/manager_user/HomeActivity.java
@@ -3,12 +3,30 @@ package com.example.manager_user;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+    ImageView chooserImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        chooserImage = findViewById(R.id.chooser_Image);
+
+        chooserImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooserUploadFile();
+            }
+        });
+
+    }
+
+    private void chooserUploadFile() {
+
+        
     }
 }
--- app/src/main/java/com/example/manager_user/Login/SignInActivity.java
+++ app/src/main/java/com/example/manager_user/Login/SignInActivity.java
@@ -2,15 +2,32 @@ package com.example.manager_user.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.manager_user.HomeActivity;
 import com.example.manager_user.R;
 
 public class SignInActivity extends AppCompatActivity {
+     Button btn_SignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btn_SignIn = findViewById(R.id.btn_signin);
+
+        btn_SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
+               startActivity(intent);
+               finish();
+            }
+        });
+
     }
 }
